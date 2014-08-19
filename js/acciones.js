7a8b7b4b28cd43e9a3303a4fc12ce76387e4a2af
Juan Carlos VO <yeankarlo@gmail.com>
2014-08-19 14:55:18 -0500

    Piano_ver_3
    Correcion del archivo acciones.

--- js/acciones.js
+++ js/acciones.js
@@ -8,6 +8,20 @@ $(document).ready(function(e){
 	// Esperar a que el dispositivo esté listo.
 	document.addEventListener("deviceready",function(){
 		// Creamos la variable "audio" a partir del constructos del objeto del plugin que especificamos en el config.xml
-		audio = window.plugin.LowLatencyAudio;
+		audio = window.plugins.LowLatencyAudio;
+		audio.preloadFX('C','audio/DO.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('D','audio/RE.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('E','audio/MI.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('F','audio/FA.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('G','audio/SOL.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('A','audio/LA.mp3',function(){},function(e){ alert('Error: ' + e); });
+		audio.preloadFX('B','audio/SI.mp3',function(){},function(e){ alert('Error: ' + e); });
+		
+		$('.nota').bind('touchstart',function(){
+			$(this).addClass('tocada');
+			audio.play($(this).attr('id'));
+		}).bind('touchend',function(){
+			$(this).removeClass('tocada');
+		});
 	},false); //deviceready
 });//ready
